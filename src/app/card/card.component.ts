b4e742272524ac56b7d159ace1891c57f19069de
Michael Crenshaw <mkrenshaw96@gmail.com>
2019-07-08 14:05:36 -0400

    Added cards for 3 differnt searches

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-card',
@@ -6,10 +7,53 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./card.component.css']
 })
 export class CardComponent implements OnInit {
+  people: boolean = false;
+  ships: boolean = false;
+  films: boolean = false;
+  person: '';
+  ship: '';
+  film: '';
+  response: '';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  // this.response ? this.people = true : null
+  // this.response ? this.ships = true : null
+  // this.response ? this.films = true : null
+
+  // if(response) {
+  //   this.people = true;
+  // }
+  // if(response) {
+  //   this.ships = true;
+  // }
+  // if(response) {
+  //   this.films = true;
+  // }
 
   ngOnInit() {
+    let peopleUrl = `https://swapi.co/api/people/?search=luke`
+    this.http.get(peopleUrl)
+      .subscribe(
+        (data: any) => {
+          this.person = data.results[0]
+        })
+
+    // let vehicleUrl = `https://swapi.co/api/vehicles/?search=sand`
+    // this.http.get(vehicleUrl)
+    //   .subscribe(
+    //     (data: any) => {
+    //       this.ship = data.results[0]
+    //     }
+    //   )
+
+    // let filmUrl = `https://swapi.co/api/films/?search=revenge`
+    // this.http.get(filmUrl)
+    //   .subscribe(
+    //     (data: any) => {
+    //       this.film = data.results[0]
+    //     }
+    //   )
   }
 
 }
