c305237e32c835e17ad95bf0b59158010b2f7058
Michael Crenshaw <mkrenshaw96@gmail.com>
2019-07-08 10:37:13 -0400

    Components made

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { CardComponent } from './card/card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    SidebarComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule,
